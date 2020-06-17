8126e8e77a7709d2000c16baaa6f2c8dabf3e637
Chris Chandler <chris@chrischandler.com>
2020-06-17 14:31:17 +0800

    Updated database config

--- server.js
+++ server.js
@@ -16,10 +16,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'localhost',
-      user : 'postgres',
-      password : 'postgres',
-      database : 'smart-brain'
+      host : 'process.env.DATABASE_URL',
+      ssl : true
     }
   });
 
