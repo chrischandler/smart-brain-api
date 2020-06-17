fcc46cdd15f355efb54a2b1370ba894abffc0b79
Chris Chandler <chris@chrischandler.com>
2020-06-17 12:05:03 +0800

    updated port to use environment variable

--- server.js
+++ server.js
@@ -46,6 +46,6 @@ app.put('/image',       (req, res) => { image.handleImage(req, res, db) });
 app.post('/imageurl',   (req, res) => { image.handleApiCall(req, res) });
 
 // Required for the server to listen for incoming requests
-app.listen(3000, () => {
-    console.log("SMART-BRAIN app is running on port 3000");
+app.listen(proces.env.PORT || 3000, () => {
+    console.log("SMART-BRAIN app is running on port ${process.env.PORT");
 })
